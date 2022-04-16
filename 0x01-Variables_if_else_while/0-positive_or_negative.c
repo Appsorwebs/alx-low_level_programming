5a4118c9ee82704e587adf0334afa52848e3cd71
“Appsorwebs” <appsorwebs@gmail.com>
2022-04-16 15:45:08 -0700

    Positive anything is better than negative nothing

--- 0x01-Variables_if_else_while/0-positive_or_negative.c
+++ 0x01-Variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- * main - assigns a random number to int n everytime
- * it executes, and prints it
- * Return: Always 0 (Success)
+ * main - entry point
+ *
+ * betty style doc for function main goes there
+ * betty there there is a decription added for main
+ * Return: 0
  */
 int main(void)
 {
@@ -13,11 +14,21 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+/* your code goes there */
 if (n > 0)
+{
+/* is the value of n positive ? */
 printf("%d is positive\n", n);
+}
 else if (n == 0)
+{
+/* is the value of n equal to zero */
 printf("%d is zero\n", n);
+}
 else if (n < 0)
+{
+/* is the value of n less than zero ? */
 printf("%d is negative\n", n);
-return (0);
+}
+return (0); /* This is a description for return value of  main */
 }
