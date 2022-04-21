fa1746e27ce5ecd4741b8ceae3390e6ca13e259a
“Appsorwebs” <appsorwebs@gmail.com>
2022-04-20 19:40:39 -0700

    I can only go one way. I've not got a reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,20 +1,23 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * print_rev - prints in reverse order
- * @s: words to be reversed
- * Return: void
+ * print_rev - print a string
+ * @s:pointer char
+ * return:void
  */
 void print_rev(char *s)
 {
-int i;
+	int i, n;
 
-
-for (i = 0; s[i] != '\0'; ++i)
-;
-i--;
-
-for (; i >= 0; --i)
-_putchar(s[i]);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	n = i - 1;
+	for (i = n; i >= 0 ; i--)
+	{
+		_putchar(s[i]);
+	}
 _putchar('\n');
+return;
 }
