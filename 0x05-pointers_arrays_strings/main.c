6653207a821cf3908c9551041ca2d0aa24779e01
“Appsorwebs” <appsorwebs@gmail.com>
2022-04-21 17:09:51 -0700

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,15 +1,49 @@
 #include "main.h"
 
-/**
- * main - check the code
+/
+ * _strcmp - a function that compares two strings
  *
- * Return: Always 0.
- */
-int main(void)
-{
-  char *str;
+ * @s1: string 1 input to compare
+ * @s2: against this other string 2
+ *
+ * Return: 0 if s1 and s2 are equal
+ *        negative integer if the stopping character
+ *                in @s1 was less than the stopping
+ *                character in @s2
+ *        positive integer if the stopping character
+ *                in @s1 was greater than the stopping
+ *                character in @s2
+*/
 
- str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
- print_rev(str);
- return (0);
+ int _strcmp(char *s1, char *s2)
+{
+int i = 0, r;
+
+ /
+* iterate through src and
+* compare it with dest
+ */
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+/**
+* if they differ by a single character
+* don't iterate further
+*/
+if (s1[i] != s2[i])
+{
+ /*
+ * return the difference between
+ * the two characters
+ */
+r = s1[i] - s2[i];
+break;
+}
+else
+{
+r = s1[i] - s2[i];
+}
+i++;
+}
+
+ return (r);
 }
