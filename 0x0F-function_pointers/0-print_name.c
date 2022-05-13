6c79445bba96687db1083196ff176720b9404a69
“Appsorwebs” <appsorwebs@gmail.com>
2022-05-13 15:22:30 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,9 @@
-#include "function_pointers.h"
-/**
- * print_name - is a function that prints a name
- * @name: pointer to name input
- * @f: pointer to void function
- * Return: void
- */
-void print_name(char *name, void (*f)(char *))
-{
-	if (name && f)
-	{
-		f(name);
-	}
-}
+#ifndef FUNC_H
+#define FUNC_H
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
